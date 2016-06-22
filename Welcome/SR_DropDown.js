c70506260375d7f40363f46dfd2323f37a5ac9b9
Nagendra Prasad <wowmylibrary@gmail.com>
2016-06-22 13:48:20 +0530

    1.Welcome to QlikSense

---
+++ Welcome/SR_DropDown.js
@@ -0,0 +1,14 @@
+define( [
+],
+function ( ) {
+
+	return {
+		paint: function ($element) {
+			//add your rendering code here
+			$element.html( "Welcome to Qlik World --author: nagendra" );
+		}
+	};
+
+} );
+
+ 
